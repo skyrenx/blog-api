648902aff0f3d2185a519ce94f27f52f6b66aa4e
Michael Gregory <michael.c.e.gregory@proton.me>
2025-08-24 20:12:32 -0500

    use postegreSQL db connection

--- src/main/java/com/michael_gregory/blog_api/dao/users/UserRepository.java
+++ src/main/java/com/michael_gregory/blog_api/dao/users/UserRepository.java
@@ -3,7 +3,6 @@ package com.michael_gregory.blog_api.dao.users;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import com.michael_gregory.blog_api.entity.users.User;;
 
